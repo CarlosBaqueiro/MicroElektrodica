d4b9936db21a929420d1e3891a8cea3d14c3c17e
Carlos Baqueiro Basto <carlosbaqueirob@gmail.com>
2024-11-23 14:41:00 -0600

    mElektrodica Uxmal (version 1.0.0)

--- Source/Grapher.py
+++ Source/Grapher.py
@@ -18,6 +18,20 @@ import numpy as np
 #from .Tools import showme
 
 class Grapher:
+    """
+    Provides methods for graphing chemical species and reactions based on the given data.
+
+    The class includes functions to plot various graphs such as species concentration,
+    reaction progress, and current density over potential. Results are visualized using
+    matplotlib to analyze chemical reactions and species behavior under different conditions.
+
+    :ivar operation: Defines the operation parameters, including potential and other settings.
+    :type operation: dict
+    :ivar species: Contains information about the different species involved in the reaction.
+    :type species: dict
+    :ivar reactions: Describes the chemical reactions present in the data.
+    :type reactions: dict
+    """
     def __init__(self, data, results):
         self.operation = data.parameters
         self.species = data.species
@@ -27,23 +41,19 @@ class Grapher:
 
     def graph_results(self, operation, species, results):
         self.plot_fval(operation, species, results)
-        #self.plot_species(operation, species, results, 'inSolution', login=False)
-        self.plot_species(operation, species, results, 'coverages', login=False, normal=False)
-        #self.plot_species(operation, species, results, 'all', login=False)
-        #self.grah_each_theta(operation, species, results, login=False)
-        #self.current(operation, results)
+        self.plot_species(operation, species, results, 'inSolution', login=True)
+        self.plot_species(operation, species, results, 'coverages', login=True)
+        self.plot_species(operation, species, results, 'all', login=True)
+        self.grah_each_theta(operation, species, results, login=True)
+        self.current(operation, results)
 
 
 
-    def plot_species(self, operation, species, results, label, login=False, normal = True):
+    def plot_species(self, operation, species, results, label, login=False):
          if label == 'coverages':
             c_species = results.theta
             legends = species.adsorbed
             plt.ylabel(r'$\theta_i$')
-            if normal:
-                sum_species = np.sum(c_species, axis=1)
-                c_species = c_species/np.max(c_species, axis=1)[:, None]
-                print(normal)
          elif label == 'inSolution':
             c_species = np.concatenate([results.c_reactants, results.c_products], axis=1)
             legends = species.reactants + species.products
@@ -52,8 +62,8 @@ class Grapher:
             c_species = np.concatenate([results.c_reactants, results.c_products, results.theta], axis=1)
             legends = species.reactants + species.products + species.adsorbed
             plt.ylabel(r'$\frac{\partial \c_i}{\partial t}\ and\ \frac{\partial \theta_i}{\partial t}$')
-         self.plot(operation, c_species, legends, login=False)
-         self.plot(operation, c_species, legends, login=True)
+         return self.plot(operation, c_species, legends, login=login)
+
 
     def grah_each_theta(self, operation, species, results, login=True):
         for i in range(len(species.adsorbed)):
@@ -82,12 +92,10 @@ class Grapher:
 
     def plot(self, operation, c_species, legends, login=False):
          plt.plot(operation.potential, c_species,  label=legends)
-         sum_species = np.sum(c_species, axis=1)
-         plt.plot(operation.potential, sum_species,  label='sum')
          plt.xlabel('Potential [V]')
          plt.grid(visible=True, which='both', axis='both',color='grey', linestyle='-',linewidth='0.2')
          plt.minorticks_on()
-         plt.legend(loc='best')
+         plt.legend(loc='lower right')
          if login: plt.yscale('log')
          plt.tight_layout()
          plt.show()
@@ -95,9 +103,8 @@ class Grapher:
     def current(self, operation, results):
         plt.plot(results.j, operation.potential)
         plt.xlabel('Current density [A/cm2]')
-        plt.ylabel('Ptencial [V]')
+        plt.ylabel('Potencial [V]')
         #plt.xscale('log')
         plt.tight_layout()
         plt.grid(visible=True, which='both', axis='both',color='grey', linestyle='-',linewidth='0.2')
         plt.show()
-
